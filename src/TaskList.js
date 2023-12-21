338e529daaadefbcd49a4e4b04449d1bf86c0de1
Noah Muhammad <noahxmuhammad@gmail.com>
2023-12-21 15:30:41 -0500

    added sample

---
+++ src/TaskList.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Task List</h2>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
