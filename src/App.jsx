bbc64ca75bd59be49b781049ba63a654b697de07
allenvarughese22 <allenvrghs2007@gmail.com>
2023-12-22 09:14:07 -0500

    added the edit and delete options

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+// Import the TaskList component at the top of your App.jsx
 import React from "react";
+import TaskList from "./TaskList"; // Assuming TaskList is in the 'components' directory
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>My Todo App</h1>
+      <TaskList />
+    </div>
+  );
 }
 
 export default App;
